e26d6a3aad8c66380f524c472d825b2ff63c75af
avolgarev <avolgarev@ussc.ru>
2024-03-18 18:26:13 +0500

    Initial commit

---
+++ UssJuniorTest/Infrastructure/Store/IStore.cs
@@ -0,0 +1,16 @@
+﻿using UssJuniorTest.Core.Models;
+
+namespace UssJuniorTest.Infrastructure.Store;
+
+/// <summary>
+/// Хранилище данных. Эмуляция БД.
+/// </summary>
+/// <remarks>ДАННЫЙ ИНТЕРФЕЙС НЕ ИЗМЕНЯТЬ.</remarks>
+public interface IStore
+{
+    IQueryable<Car> GetAllCars();
+    
+    IQueryable<Person> GetAllPersons();
+    
+    IQueryable<DriveLog> GetAllDriveLogs();
+}
